ce0f3233e31e78b058a3cd2826274061494f49ed
Marcelo Jaimes <marceloeziojaimes@gmail.com>
2022-05-28 19:29:05 -0400

    poor changes

--- src/main/java/bo/bosque/com/impexpap/dao/IRelEmpEmpr.java
+++ src/main/java/bo/bosque/com/impexpap/dao/IRelEmpEmpr.java
@@ -1,4 +1,15 @@
 package bo.bosque.com.impexpap.dao;
+import java.util.List;
+import bo.bosque.com.impexpap.model.RelEmplEmpr;
+
+
 
 public interface IRelEmpEmpr {
+
+    /**
+     * Obtendra el lista de relaciones laborales de una empleado
+     * @param codEmpleado
+     * @return
+     */
+    List<RelEmplEmpr> obtenerRelacionesLaborales(int codEmpleado );
 }
--- src/main/java/bo/bosque/com/impexpap/dao/RelEmpEmprDAO.java
+++ src/main/java/bo/bosque/com/impexpap/dao/RelEmpEmprDAO.java
@@ -1,7 +1,20 @@
 package bo.bosque.com.impexpap.dao;
 
+import bo.bosque.com.impexpap.model.RelEmplEmpr;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class RelEmpEmprDAO implements IRelEmpEmpr {
+
+
+    /**
+     * Procedimiento para obtener el listado de relaciones laborales de un empleado
+     * @param codEmpleado
+     * @return
+     */
+    public List<RelEmplEmpr> obtenerRelacionesLaborales( int codEmpleado ) {
+        return null;
+    }
 }
