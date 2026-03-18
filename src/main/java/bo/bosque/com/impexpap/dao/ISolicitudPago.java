f53ab3a401bdf2d8714528ec33df7b4d9a9180ff
Marcelo Jaimes <marceloeziojaimes@gmail.com>
2026-03-18 13:35:24 -0400

    test SolicitudPago

--- src/main/java/bo/bosque/com/impexpap/dao/ISolicitudPago.java
+++ src/main/java/bo/bosque/com/impexpap/dao/ISolicitudPago.java
@@ -1,9 +1,11 @@
 package bo.bosque.com.impexpap.dao;
 
 
+import bo.bosque.com.impexpap.dto.SolicitudPagoDto;
 import bo.bosque.com.impexpap.model.SolicitudPago;
 import bo.bosque.com.impexpap.utils.RespuestaSp;
 
+import java.util.Date;
 import java.util.List;
 
 public interface ISolicitudPago {
@@ -11,17 +13,29 @@ public interface ISolicitudPago {
 
     /**
      * Procedimiento para registrar las Solicitudes de Pago
+     *
      * @param mb
      * @param acc
      * @return
      */
-    RespuestaSp registrarSolicitudPago(SolicitudPago mb, String acc );
+    RespuestaSp registrarSolicitudPago(SolicitudPago mb, String acc);
 
     /**
      * Procedimiento para obtener las Solicitudes de Pago por Id
+     *
      * @param idSolicitud
      * @return
      */
-    List<SolicitudPago> obtenerSolicitudPago( long idSolicitud );
+    List<SolicitudPago> obtenerSolicitudPago(long idSolicitud);
+
+    /**
+     * Procedimiento para obtener las Solicitudes de Pago entre fechas
+     *
+     * @param fechaInicio
+     * @param fechaFin
+     * @return
+     */
+    List<SolicitudPagoDto> reporteSolicitudesXFecha(Date fechaInicio, Date fechaFin);
+
 
 }
