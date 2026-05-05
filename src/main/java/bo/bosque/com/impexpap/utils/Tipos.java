d189fad9728608be0cb25678749f75e39aa47ac5
Marcelo Jaimes <marceloeziojaimes@gmail.com>
2026-05-05 14:40:09 -0400

    Agregande un elemento a tipos

--- src/main/java/bo/bosque/com/impexpap/utils/Tipos.java
+++ src/main/java/bo/bosque/com/impexpap/utils/Tipos.java
@@ -272,6 +272,7 @@ public class Tipos {
         listaTemp.add(new Tipos("CDET","CHIP DETERIORADO",62));
         listaTemp.add(new Tipos("CSIM","CAMBIO eSIM",62));
         listaTemp.add(new Tipos("SP","SALIDA PERSONAL",62));
+        listaTemp.add(new Tipos("RL","RECUPERACION LINEA",62));
         return listaTemp;
     }
     /**
