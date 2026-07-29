cf0860959ba99bdc974e3cbe176ca5c109834592
Marcelo Jaimes <marceloeziojaimes@gmail.com>
2026-07-29 12:59:10 -0400

    Agregando nuevos parametros para AnticipoDao.java

--- src/main/java/bo/bosque/com/impexpap/dao/AnticipoDao.java
+++ src/main/java/bo/bosque/com/impexpap/dao/AnticipoDao.java
@@ -18,44 +18,52 @@ import java.util.Map;
 public class AnticipoDao implements IAnticipo {
     private final SpHelper spHelper;
 
-    public AnticipoDao (SpHelper spHelper){this.spHelper = spHelper;}
+    public AnticipoDao(SpHelper spHelper) {
+        this.spHelper = spHelper;
+    }
 
     /**
      * procedimiento para registrar o actualizar un anticipo
+     * 
      * @param an
      * @param acc
      * @return
      */
     @Override
-    public RespuestaSp registrarAnticipo(Anticipo an,String acc){
-        return this.spHelper.ejecutarAbm("p_abm_Anticipo",an,acc);
+    public RespuestaSp registrarAnticipo(Anticipo an, String acc) {
+        return this.spHelper.ejecutarAbm("p_abm_Anticipo", an, acc);
     }
+
     @Override
-    public List<Anticipo> obtenerAnticiposRegistrados(Anticipo an){
-        return this.spHelper.ejecutarListado("p_list_Anticipo",an,"B",Anticipo.class);
+    public List<Anticipo> obtenerAnticiposRegistrados(Anticipo an) {
+        return this.spHelper.ejecutarListado("p_list_Anticipo", an, "B", Anticipo.class);
     }
 
     /**
      * LISTA ANTICIPOS UNIFICADOS SAP - BOSQUE
+     * 
      * @param an
      * @return
      */
     @Override
-    public List<Anticipo> anticiposUnificados(Anticipo an){
-        return this.spHelper.ejecutarListado("p_list_Anticipo",an,"C",Anticipo.class);
+    public List<Anticipo> anticiposUnificados(Anticipo an) {
+        return this.spHelper.ejecutarListado("p_list_Anticipo", an, "C", Anticipo.class);
     }
 
     /**
      * LISTADO DE LOS ANTICIPOS REGISTRADOS EN SAP LISTOS PARA IMPORTAR AL BOSQUE.
+     * 
      * @param an
      * @return
      */
     @Override
-    public List<Anticipo> obtenerAnticiposSAP(Anticipo an){
-        return this.spHelper.ejecutarListado("p_list_Anticipo",an,"A",Anticipo.class);
+    public List<Anticipo> obtenerAnticiposSAP(Anticipo an) {
+        return this.spHelper.ejecutarListado("p_list_Anticipo", an, "A", Anticipo.class);
     }
+
     /**
      * Obtendra una lista de tipo de renovacion chip tigo
+     * 
      * @return
      */
     public List<Tipos> listTipoAsignacion() {
@@ -64,6 +72,7 @@ public class AnticipoDao implements IAnticipo {
 
     /**
      * CALCULO DE MONTOS (PREVISUALIZACION)
+     * 
      * @param an
      * @return
      */
@@ -71,18 +80,19 @@ public class AnticipoDao implements IAnticipo {
     public List<AnticipoPreview> previsualizarAsignacion(Anticipo an) {
         // ✅ Usar Map explícito con SOLO los parámetros necesarios
         Map<String, Object> params = new LinkedHashMap<>();
-        params.put("codEmpresa",   an.getCodEmpresa());
-        params.put("debe",         an.getDebe());
+        params.put("codEmpresa", an.getCodEmpresa());
+        params.put("debe", an.getDebe());
+        params.put("haber", an.getHaber());
         params.put("xmlEmpleados", an.getXmlEmpleados());
         // Opcionales para que el SP no falle en validación:
-        if (an.getNumAsiento() != null) params.put("numAsiento", an.getNumAsiento());
-        if (an.getFechaAsiento() != null) params.put("fechaAsiento", an.getFechaAsiento());
-        //if (an.getAudUsuarioI()) params.put("audUsuarioI", an.getAudUsuarioI());
+        if (an.getNumAsiento() != null)
+            params.put("numAsiento", an.getNumAsiento());
+        if (an.getFechaAsiento() != null)
+            params.put("fechaAsiento", an.getFechaAsiento());
+        // if (an.getAudUsuarioI()) params.put("audUsuarioI", an.getAudUsuarioI());
 
         return this.spHelper.ejecutarListado(
-                "p_abm_Anticipo", params, "P", AnticipoPreview.class
-        );
+                "p_abm_Anticipo", params, "P", AnticipoPreview.class);
     }
-
 
 }
